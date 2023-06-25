c1b2bbb6a85108cfe9904c2677a1211905ba42fb
Sergey Rodovinsky <srodovinsky@pushnami.com>
2023-06-25 13:40:19 -0400

    Using 'address' instead of 'endpoint' to skip port

--- infrastructure/taskdef.tf
+++ infrastructure/taskdef.tf
@@ -1,3 +1,6 @@
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/devops"
+}
 resource "aws_ecs_task_definition" "node_js" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
@@ -25,14 +28,14 @@ resource "aws_ecs_task_definition" "node_js" {
       "logDriver": "awslogs",
       "options": {
         "awslogs-region" : "${var.aws_region}",
-        "awslogs-group" : "devops",
+        "awslogs-group" : "/devops",
         "awslogs-stream-prefix" : "node_js"
       }
     },
     "environment": [
       {
         "name": "DB_HOST",
-        "value": "${aws_db_instance.mysql.endpoint}"
+        "value": "${aws_db_instance.mysql.address}"
       },
       {
         "name": "DB_USER",
